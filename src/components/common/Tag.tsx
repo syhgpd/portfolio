fdb610f578bf243ddef6eb033de3950e2cb2031c
hyeyeonchurros <hyeyoenchurros@naver.com>
2023-03-23 16:11:56 +0900

    common tag 컴포넌트 추가

---
+++ src/components/common/Tag.tsx
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+
+interface Props {
+  children: string;
+  isActive: boolean;
+  onClick?: () => void;
+}
+
+const Tag = ({ children, isActive, onClick }: Props) => {
+  return (
+    <TagWraaper isActive={isActive} onClick={onClick}>
+      {children}
+    </TagWraaper>
+  );
+};
+
+const TagWraaper = styled.div<{ isActive: boolean }>`
+  padding: 4px 14px;
+  font-size: 16px;
+  background-color: ${({ isActive, theme }) =>
+    isActive ? theme.pink : theme.white};
+  color: ${({ theme }) => theme.white};
+  border-radius: 20px;
+  color: ${({ isActive, theme }) => (isActive ? theme.white : theme.pink)};
+  border: 1px solid ${({ isActive, theme }) => isActive && theme.pink};
+`;
+
+export default Tag;
