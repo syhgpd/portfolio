810747268be45bcdb8c30320f1b4f23c58308c3b
hyeyeonchurros <hyeyoenchurros@naver.com>
2023-03-23 11:54:24 +0900

    chore : theme color 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,11 @@
 export const theme = {
   white: "#FFFFFF",
+  gray300: "#F6F6F6",
+  gray500: "#DEDEDE",
+  gray700: "#8D8D8D",
+  gray900: "#2F2F2F",
   black: "#000000",
-  lightPink: "#F2E9EB",
+  lightPink: "#FCF4F6",
   pink: "#DD8EA4",
-  yellow: "#FEDD89",
   charcoal: "#31383F",
 };
